75df2ce656c187f528d9a5225e2406bc3fadda6b
Micah Elliott <micah.elliott@fundingcircle.com>
2020-06-01 14:07:56 -0700

    Fix FZF completions, restructure logs as log/yr/wk/isodate.log

--- setup.zsh
+++ setup.zsh
@@ -1,14 +1,15 @@
 ### FZF Completion
-# Enable completion of sqlstash queries.
+# Enable completion of log files
 # % dunnit ,<TAB>
 # > log/20200530.log
 # > ...
-_fzf_complete_sqlstash() {
-    qfiles=( log/*.log)
-    _fzf_complete --multi --reverse \
-                  --preview 'bat -l sql --color always queries/query-{}.tpl.sql' \
+_fzf_complete_dunnit() {
+    logfiles=( log/*/*/*.log )
+    _fzf_complete --multi  \
+                  --preview 'bat -l log --color always log/$(date +%Y)/{}.log' \
                   --prompt="log> " -- "$@" \
-                  < <( for f in $logfiles; sed -e 's#.*/log-##' -e 's/\..*//' <<<$f ) }
+                  < <(ls $logfiles | sed -e 's#log/202./##' -e 's/\.log//' |tac) }
+                  # < <( for f in $logfiles; sed -e 's#.*/log-##' -e 's/\..*//' <<<$f ) }
 
 # Micah likes this as a shortcut.
 FZF_COMPLETION_TRIGGER=,
